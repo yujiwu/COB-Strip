287350eb6138b1bc9161caca5ac426700dcbecda
Yujie Wu <yujiewu@YujiedeiMac.local>
2023-11-22 20:51:48 +0800

    V2.1

--- COB-Strip.ino
+++ COB-Strip.ino
@@ -14,6 +14,20 @@ rl,gl,bl pins controls the lower led strip
 #define gl 5
 #define bl 6
 
+// red 255, 0, 0
+// orange 255, 102, 0
+// green 0, 255, 0
+// indigo 3, 244, 252
+// blue 0, 0, 255
+// purple 191, 0, 255
+// white 220, 220, 220
+
+int colorR[] = {255,255,0,3,0,191,220};
+int colorG[] = {0,102,255,244,0,0,220};
+int colorB[] = {0,0,0,252,255,255,220};
+int i = 0; 
+int j = 0; 
+
 void setup() {
   pinMode(ru, OUTPUT);
   pinMode(gu, OUTPUT);
@@ -24,29 +38,45 @@ void setup() {
 }
 
 void loop() {
-  // Phase 1
-  // purple 191, 0, 255
-  fadeInUpper(191, 0, 255, 5);
-  // blue 0, 0, 255
-  fadeInLower(0, 0, 255, 5);
-  // remain turned on for n/1000 seconds
+
+  while(i==j){
+    i = random(0,7);
+    j = random(0,7);
+  }
+  fadeInUpper(colorR[i], colorG[i], colorB[i], 5);
+  fadeInLower(colorR[j], colorG[j], colorB[j], 5);
   delay(180000);
-  fadeOutUpper(191, 0, 255, 5);
-  fadeOutLower(0, 0, 255, 5);
+  fadeOutUpper(colorR[i], colorG[i], colorB[i], 5);
+  fadeOutLower(colorR[j], colorG[j], colorB[j], 5);
   turnOff();
   delay(1000);
 
-  // Phase 2
-  // green 0, 255, 0
-  fadeInUpper(0, 255, 0, 5);
-  // orange 255, 102, 0
-  fadeInLower(255, 102, 0, 5);
-  // remain turned on for n/1000 seconds
-  delay(180000);
-  fadeOutUpper(0, 255, 0, 5);
-  fadeOutLower(255, 102, 0, 5);
-  turnOff();
-  delay(1000);
+  i = 0;
+  j = 0;
+
+  // // Phase 1
+  // // purple 191, 0, 255
+  // fadeInUpper(191, 0, 255, 5);
+  // // blue 0, 0, 255
+  // fadeInLower(0, 0, 255, 5);
+  // // remain turned on for n/1000 seconds
+  // delay(180000);
+  // fadeOutUpper(191, 0, 255, 5);
+  // fadeOutLower(0, 0, 255, 5);
+  // turnOff();
+  // delay(1000);
+
+  // // Phase 2
+  // // green 0, 255, 0
+  // fadeInUpper(0, 255, 0, 5);
+  // // orange 255, 102, 0
+  // fadeInLower(255, 102, 0, 5);
+  // // remain turned on for n/1000 seconds
+  // delay(180000);
+  // fadeOutUpper(0, 255, 0, 5);
+  // fadeOutLower(255, 102, 0, 5);
+  // turnOff();
+  // delay(1000);
  
 
 }
